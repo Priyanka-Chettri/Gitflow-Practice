989ca6c2fa4f05f99e0c45c840b7a040b99ec797
Priyanka-Chettri <priyankachettri85@gmail.com>
2025-01-28 16:57:32 +0530

    added release branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
         A Develop branch in gitflow
         <h1>This is a feature branch add_heading branch</h1>
+        <h4>Bug fix release branch</h4>
       </div>
      
     </>
